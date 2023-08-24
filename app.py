d2928cf3d8c961626f3ad95d89cf7dfaff9392cf
ian-kim-0513 <105325024+ian-kim-0513@users.noreply.github.com>
2023-08-24 17:16:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # from streamlit_extras.add_vertical_space import add_vertical_space
-from PyPDF2 import PdfReader
+# from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
