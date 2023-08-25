9c67888450676a0b87ebef5647f1e39c5bf62117
ian-kim-0513 <105325024+ian-kim-0513@users.noreply.github.com>
2023-08-25 14:54:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,22 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn = connect(credentials=credentials)
 
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
+
+
 with st.sidebar:
     st.title('🤗💬 Mooiatti Chat App')
     st.markdown('''
