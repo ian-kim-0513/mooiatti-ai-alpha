e526099914e1b03869360d37a08254e130bb0bcb
ian-kim-0513 <105325024+ian-kim-0513@users.noreply.github.com>
2023-08-26 14:45:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,11 +54,6 @@ def main():
     st.write("모이아띠 AI 에게 물어보세요")
     
     st.write("GLoading ...")
-    loader = GoogleDriveLoader(
-        folder_id="1x_Ze95L2lBfoojCA8tj6o56lnw0_-Hiy",
-        recursive=False
-    )
-    docs = loader.load()
     st.write("Loading Done ...")
 
 
