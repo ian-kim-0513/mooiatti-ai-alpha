22338611a1401b9154072c6e32264e42b1bb5b1b
ian-kim-0513 <105325024+ian-kim-0513@users.noreply.github.com>
2023-08-26 18:05:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def main():
     
     sheet_url = st.secrets["private_gsheets_url"]
     rows = run_query(f'SELECT * FROM "{sheet_url}"')
-    sf.write("rows retrieved")
+    st.write("rows retrieved")
 
     st.write("RecursiveCharacterTextSplitter ...")
     text_splitter = RecursiveCharacterTextSplitter(
